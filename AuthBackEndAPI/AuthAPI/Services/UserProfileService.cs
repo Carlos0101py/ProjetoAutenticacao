7770ead2404b288dab8d3bdd368f09d439917366
carlos <carloserico71@gmail.com>
2025-04-12 00:38:18 -0300

    feat: implementa o serviço de alteração de dados do usuario

--- AuthBackEndAPI/AuthAPI/Services/UserProfileService.cs
+++ AuthBackEndAPI/AuthAPI/Services/UserProfileService.cs
@@ -1,4 +1,6 @@
 using AuthAPI.DTOs;
+using AuthAPI.Helper;
+using AuthAPI.Models;
 using AuthAPI.Repositories;
 
 namespace AuthAPI.Service
@@ -6,9 +8,11 @@ namespace AuthAPI.Service
     public class UserProfileService
     {
         private readonly IUserRepository _userRepository;
-        public UserProfileService(IUserRepository userRepository)
+        private readonly IResponseBuilder<User> _responseBuilder;
+        public UserProfileService(IUserRepository userRepository, IResponseBuilder<User> responseBuilder)
         {
             _userRepository = userRepository;
+            _responseBuilder = responseBuilder;
         }
 
         
@@ -38,6 +42,41 @@ namespace AuthAPI.Service
                     Message = "Usuario deletado com sucesso!",
                     Date = user
                 };
+            }
+            catch (Exception ex)
+            {
+                return new ResponseDTO
+                {
+                    Success = false,
+                    Message = $"Ocorreu um erro interno: {ex.Message}",
+                    Date = null
+                };
+            }
+        }
+
+        public async Task<ResponseDTO> ChangeUserInformation(Guid id, ChengeAccountDTO chengeAccountDTO)
+        {
+
+            try
+            {
+                var user = await _userRepository.GetById(id);
+
+                if (user == null)
+                {
+                    return _responseBuilder.NotFound("Usuario não encontrado, Verifique e tente novamente.");
+                }
+
+                if (chengeAccountDTO.Password != chengeAccountDTO.RePassword)
+                {
+                    return _responseBuilder.Conflict("As senhas inseridas não coincidem, verifique e tente novamente.");
+                }
+
+                user.UserName = chengeAccountDTO.UserName;
+                user.Password = chengeAccountDTO.Password;
+
+                await _userRepository.Change();
+
+                return _responseBuilder.OK(null, "Informações alteradas com sucesso!");
             }
             catch (Exception ex)
             {
